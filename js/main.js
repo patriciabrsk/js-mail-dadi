cda2f8cde82203dca599ea140c7597b76ca245b2
patriciabrsk <patriciabresku@gmail.com>
2022-02-25 18:58:26 +0100

    array iteration

--- js/main.js
+++ js/main.js
@@ -5,30 +5,29 @@ const emailsList = [
     'third@email.com', 
     'fourth@email.com',
     'fifth@email.com',
+    'sixth@email.com',
 ];
 
 document.getElementById('enter').addEventListener('click',
-    function(inputName, ) {
+    function() {
 
-        let statusNo = "Email is not listed!";
-        let statusYes = "Welcome!"
+        let notListed = "Email is not listed!";
+        let welcome = "Welcome!"
         let message = document.getElementById('message');
 
         // Chiedi all’utente la sua email
         const inputEmail = document.querySelector('.email').value;
-        // console.log(inputEmail);
+
         // controlla che sia nella lista di chi può accedere,
         // for
         for (let i = 0; i < emailsList.length; i++) {
-
-            if (emailsList[i] === inputEmail) {
-                message.innerHTML = statusYes;
-                message.classList.remove('invisible');
-                message.classList.add('visible');
+            if (emailsList[i] == inputEmail) {
+                message.innerHTML = welcome;
+                message.classList.add('text-success');
             } else {
-                message.innerHTML = statusNo;
-                message.classList.remove('invisible');
-                message.classList.add('visible');
+                message.innerHTML = notListed;
+                // message.classList.remove('text-success');
+                message.classList.add('text-danger');
             }
         }
     });
@@ -56,8 +55,6 @@ document.getElementById('enter').addEventListener('click',
                 result.innerHTML = "&#128079; You win! &#128079;";
             }
         });
-
-        
 
 
 
